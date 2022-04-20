0bb37d1deba5da3dca93db6df94924fc261746a1
lutogb27 <lutogb27@yahoo.co.jp>
2022-04-20 21:09:15 +0900

    修正18

---
+++ resources/js/form.js
@@ -0,0 +1,106 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // 「送信」ボタンの要素を取得
+    const submit = document.querySelector('.submit');
+    
+    // 「送信」ボタンの要素にクリックイベントを設定する
+    submit.addEventListener('click', (e) => {
+        // デフォルトアクションをキャンセル
+        e.preventDefault();
+
+        // 「お名前」入力欄の空欄チェック
+        // フォームの要素を取得
+        const name = document.querySelector('#name');
+        // エラーメッセージを表示させる要素を取得
+        const errMsgName = document.querySelector('.err-msg-name');
+        if(!name.value){
+            // クラスを追加(エラーメッセージを表示する)
+            errMsgName.classList.add('form-invalid');
+            // エラーメッセージのテキスト
+            errMsgName.textContent = 'お名前が入力されていません';
+            // クラスを追加(フォームの枠線を赤くする)
+            name.classList.add('input-invalid');
+            // 後続の処理を止める
+            return;
+        }else{
+            // エラーメッセージのテキストに空文字を代入
+            errMsgName.textContent ='';
+            // クラスを削除
+            name.classList.remove('input-invalid');
+      }
+
+      const kana = document.querySelector('#kana');
+        const errMsgKana = document.querySelector('.err-msg-kana');
+        if(!kana.value){
+            errMsgKana.classList.add('form-invalid');ト
+            errMsgKana.textContent = 'フリガナが入力されていません';
+            kana.classList.add('input-invalid');
+            return;
+        }else{
+            errMsgKana.textContent ='';
+            kana.classList.remove('input-invalid');
+      }
+
+       // 「年齢」入力欄の形式チェック
+        const age = document.querySelector('#age');
+        const errMsgAge = document.querySelector('.err-msg-age');
+        // 年齢が2文字以上の半角英数字であるかどうかのチェック
+        if(!age.value.match(/^([a-zA-Z0-9]{2,})$/)){
+            errMsgAge.classList.add('form-invalid');
+            errMsgAge.textContent = '半角英数字2文字以上で入力してください';
+            age.classList.add('input-invalid');
+            return;
+        }else{
+            errMsgAge.textContent ='';
+            age.classList.remove('input-invalid');
+      }
+
+      const gender = document.querySelector('#gender');
+        const errMsgGender = document.querySelector('.err-msg-gender');
+        if(!gender.value){
+            errMsgGender.classList.add('form-invalid');ト
+            errMsgGender.textContent = '性別が入力されていません';
+            gender.classList.add('input-invalid');
+            return;
+        }else{
+            errMsgGender.textContent ='';
+            gender.classList.remove('input-invalid');
+      }
+
+      const zipcode = document.querySelector('#zipcode');
+      const errMsgZipcode = document.querySelector('.err-msg-zipcode');
+      if(!zipcode.value.match(/^([a-zA-Z0-9]{7,})$/)){
+            errMsgZipcode.classList.add('form-invalid');
+            errMsgZipcode.textContent = '半角英数字7文字以上で入力してください';
+            zipcode.classList.add('input-invalid');
+            return;
+        }else{
+            errMsgZipcode.textContent ='';
+            zipcode.classList.remove('input-invalid');
+      }
+
+      const address = document.querySelector('#address');
+        const errMsgAddress = document.querySelector('.err-msg-address');
+        if(!address.value){
+            errMsgAddress.classList.add('form-invalid');ト
+            errMsgAddress.textContent = '住所が入力されていません';
+            address.classList.add('input-invalid');
+            return;
+        }else{
+            errMsgAddress.textContent ='';
+            address.classList.remove('input-invalid');
+      }
+
+      const body = document.querySelector('#body');
+        const errMsgBody = document.querySelector('.err-msg-body');
+        if(!body.value){
+            errMsgBody.classList.add('form-invalid');ト
+            errMsgBody.textContent = 'お問い合わせ内容が入力されていません';
+            body.classList.add('input-invalid');
+            return;
+        }else{
+            errMsgBody.textContent ='';
+            body.classList.remove('input-invalid');
+      }
+    }, false);
+}, false);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,51 +26,79 @@
                     <table>
                     @csrf
                         <tr>
+                            <th></th>
+                            <td><div class="err-msg-name"></div></td>
+                        </tr>
+                        <tr>
                             <th>名前</th>
                             <td>
-                                <input type="text" name="name">
+                                <input type="text" name="name" id="name" placeholder="漢字">
                             </td>
                         </tr>
 
+                        <tr>
+                            <th></th>
+                            <td><div class="err-msg-kana"></div></td>
+                        </tr>
                         <tr>
                             <th>フリガナ</th>
                             <td>
-                                <input type="text" name="kana">    
+                                <input type="text" name="kana" id="kana" placeholder="フリガナ">    
                             </td>
                         </tr>
 
+                        <tr>
+                            <th></th>
+                            <td><div class="err-msg-age"></div></td>
+                        </tr>
                         <tr>
                             <th>年齢</th>
                             <td>
-                                <input type="text" name="age">
+                                <input type="text" name="age" id="age" placeholder="年齢">
                             </td>
                         </tr>
 
+                        <tr>
+                            <th></th>
+                            <td><div class="err-msg-gender"></div></td>
+                        </tr>
                         <tr>
                             <th>性別</th>    
                             <td>
-                                <input type="text" name="gender"> 
+                                <input type="text" name="gender" id="gender" placeholder="性別"> 
                             </td>
                         </tr>
 
+                        <tr>
+                            <th></th>
+                            <td><div class="err-msg-zipcode"></div></td>
+                        </tr>
                         <tr>
                             <th>郵便番号</th>
                             <td>
-                                <input type="text" name="zipcode">
-                            </td>
-                        </tr>
-
-                        <tr>    
-                            <th>住所</th>
-                            <td>
-                                <input type="text" name="address"> 
+                                <input type="text" name="zipcode" id="zipcode" placeholder="郵便番号">
                             </td>
                         </tr>
 
                         <tr>
+                            <th></th>
+                            <td><div class="err-msg-address"></div></td>
+                        </tr>
+                        <tr>    
+                            <th>住所</th>
+                            <td>
+                                <input type="text" name="address" id="address" placeholder="住所"> 
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th></th>
+                            <td><div class="err-msg-body"></div></td>
+                        </tr>
+                        <tr>
                             <th>お問い合わせ内容</th>
                             <td>
-                                <textarea name="body"></textarea>
+                                <textarea name="body" id="body" placeholder="内容"></textarea>
                             </td>
                         </tr>
                     </table>
