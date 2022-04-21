5e72cf8ea52e71f07eb0dbf19104e993ad995e72
lutogb27 <lutogb27@yahoo.co.jp>
2022-04-21 21:11:02 +0900

    修正19

--- resources/js/form.js
+++ resources/js/form.js
@@ -27,9 +27,9 @@ window.addEventListener('DOMContentLoaded', () => {
             errMsgName.textContent ='';
             // クラスを削除
             name.classList.remove('input-invalid');
-      }
+        }
 
-      const kana = document.querySelector('#kana');
+        const kana = document.querySelector('#kana');
         const errMsgKana = document.querySelector('.err-msg-kana');
         if(!kana.value){
             errMsgKana.classList.add('form-invalid');ト
@@ -39,23 +39,23 @@ window.addEventListener('DOMContentLoaded', () => {
         }else{
             errMsgKana.textContent ='';
             kana.classList.remove('input-invalid');
-      }
+        }
 
        // 「年齢」入力欄の形式チェック
         const age = document.querySelector('#age');
         const errMsgAge = document.querySelector('.err-msg-age');
         // 年齢が2文字以上の半角英数字であるかどうかのチェック
-        if(!age.value.match(/^([a-zA-Z0-9]{2,})$/)){
+        if(!age.value.match(/^([a-zA-Z0-9]{2})$/)){
             errMsgAge.classList.add('form-invalid');
-            errMsgAge.textContent = '半角英数字2文字以上で入力してください';
+            errMsgAge.textContent = '半角英数字2文字で入力してください';
             age.classList.add('input-invalid');
             return;
         }else{
             errMsgAge.textContent ='';
             age.classList.remove('input-invalid');
-      }
+        }
 
-      const gender = document.querySelector('#gender');
+        const gender = document.querySelector('#gender');
         const errMsgGender = document.querySelector('.err-msg-gender');
         if(!gender.value){
             errMsgGender.classList.add('form-invalid');ト
@@ -65,21 +65,21 @@ window.addEventListener('DOMContentLoaded', () => {
         }else{
             errMsgGender.textContent ='';
             gender.classList.remove('input-invalid');
-      }
+        }
 
-      const zipcode = document.querySelector('#zipcode');
-      const errMsgZipcode = document.querySelector('.err-msg-zipcode');
-      if(!zipcode.value.match(/^([a-zA-Z0-9]{7,})$/)){
+        const zipcode = document.querySelector('#zipcode');
+        const errMsgZipcode = document.querySelector('.err-msg-zipcode');
+        if(!zipcode.value.match(/^([a-zA-Z0-9]{7})$/)){
             errMsgZipcode.classList.add('form-invalid');
-            errMsgZipcode.textContent = '半角英数字7文字以上で入力してください';
+            errMsgZipcode.textContent = '半角英数字7文字で入力してください';
             zipcode.classList.add('input-invalid');
             return;
         }else{
             errMsgZipcode.textContent ='';
             zipcode.classList.remove('input-invalid');
-      }
+        }
 
-      const address = document.querySelector('#address');
+        const address = document.querySelector('#address');
         const errMsgAddress = document.querySelector('.err-msg-address');
         if(!address.value){
             errMsgAddress.classList.add('form-invalid');ト
@@ -89,9 +89,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }else{
             errMsgAddress.textContent ='';
             address.classList.remove('input-invalid');
-      }
+        }
 
-      const body = document.querySelector('#body');
+        const body = document.querySelector('#body');
         const errMsgBody = document.querySelector('.err-msg-body');
         if(!body.value){
             errMsgBody.classList.add('form-invalid');ト
@@ -101,6 +101,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }else{
             errMsgBody.textContent ='';
             body.classList.remove('input-invalid');
-      }
+        }
     }, false);
 }, false);
